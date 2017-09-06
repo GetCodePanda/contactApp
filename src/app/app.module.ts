2e73324c87c833daac9b42cf785c993b8eca826c
GetCodePanda <hicodepanda@gmail.com>
2017-09-06 19:54:39 +0530

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Angular Material
+import { MdToolbarModule, MdSidenavModule, MdGridListModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdSidenavModule,
+    MdGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+/* angular materail init..
+import hammer js because Some angular matrial components
+(md-slide-toggle, md-slider, mdTooltip) rely on HammerJS for gesture
+*/
+
+import 'hammerjs';
+
 if (environment.production) {
   enableProdMode();
 }
