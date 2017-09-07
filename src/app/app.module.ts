eb58e0521003a9d4ff42455ed38c9af74f8ea590
GetCodePanda <hicodepanda@gmail.com>
2017-09-07 16:13:33 +0530

    setup a sidenav items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
 
 //Angular Material
-import { MdToolbarModule, MdSidenavModule, MdGridListModule, MdIconModule } from '@angular/material';
+import { 
+  MdToolbarModule, 
+  MdSidenavModule, 
+  MdGridListModule, 
+  MdIconModule, 
+  MdMenuModule } from '@angular/material';
 
+import appRoutes from './routes'
+
+// Application components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 
 @NgModule({
@@ -16,14 +26,17 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
     MdSidenavModule,
     MdGridListModule,
-    MdIconModule
+    MdIconModule,
+    MdMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
